de125669c59ef547caa114be69badfda06f488bc
Janynne Gomes <janynnegomes@gmail.com>
2018-11-22 19:17:25 -0200

    Adicionadas f declarÃrações das funlçoe para CRUD básico

---
+++ API/functions/banco.js
@@ -0,0 +1,5 @@
+const functions = require('firebase-functions');
+
+
+
+   
--- API/functions/index.js
+++ API/functions/index.js
@@ -10,6 +10,9 @@ var firestore = admin.firestore();
 /******************************************************/
 // Instância das coleções(tabelas) na base de dados
 var Livros = firestore.collection('livros');
+var Reservas = firestore.collection('reservas');
+
+
 
 /******************************************************/
 
@@ -37,16 +40,17 @@ exports.chatbot = functions.https.onRequest((request, response) => {
                 // Implementar a lógica de leitura dos dados do Firestore
                 // Obtém todos os registros da coleção 'livros' (análogo ao select * from dos bancos de dados relacionais)
                 // DOC: https://firebase.google.com/docs/firestore/query-data/get-data?hl=pt-br 
+                
                 Livros.where("Categoria", "==", params.categoria)
                 .get().then(function(resultado) {                        
                    
                     var resposta = '';
-                    
+
                     // Percorre a lista de livros encontrados
                     resultado.forEach(function(livro) {    
                         contResult++;                        
                         // Monta o texto linha a linha da conversa com informações dos livros
-                        resposta += `Nº ${contResult} é ${livro.data().Titulo} escrito por ${livro.data().Autor}. \n \n`;                         
+                        resposta += `- Nº ${contResult} é ${livro.data().Titulo} escrito por ${livro.data().Autor}. \n \n`;                         
                     });
 
                     if(contResult < 1){
@@ -58,10 +62,12 @@ exports.chatbot = functions.https.onRequest((request, response) => {
                     // Introduz a resposta, preparando para a lista de informações que vem a seguir
                     resposta = 
                         `Entendido! Procurei aqui e encontrei ${contResult} livro(s) de ${params.categoria}. Veja só: \n \n`+
-                        resposta;
+                        `**************************** \n \n`+
+                        resposta+
+                        `**************************** \n \n`;
 
                     // Encerra o diálogo pedindo que o usuário tome uma decisão
-                    resposta += "\n Você gostaria de reservar qual deles? Preciso que me diga o número do livro.";
+                    resposta += `\n Você gostaria de reservar qual deles? Preciso que me diga o número do livro.`;
 
                     // Envia a resposta dentro do formato reconhecido pelo DialogFlow
                     response.send({
@@ -96,3 +102,81 @@ exports.chatbot = functions.https.onRequest((request, response) => {
     } 
 
    });   
+  
+
+
+//*********************************************************|
+//             CRUD do Livro    - PARA APLICAÇÃO           |
+//*********************************************************|
+
+exports.ListarLivros = functions.https.onRequest((request, response) =>    {
+
+        // Objeto contendo parametros vindos do DialogFlow
+        let categoria = request.body.categoria;
+
+        response.send({resposta:request.body.categoria});
+
+        var list = [];
+
+        var _resposta =  Livros.where("Categoria", "==", "programação").get();
+
+        _resposta.then(function(resultado) { 
+            resultado.forEach(function(livro) {                   
+                console.log(livro.id, " => ", livro.data());
+                list.push({id: livro.id, dados: livro.data()});                   
+            });
+
+            response.send({resultado:list});
+        })
+        .catch((e=>{
+            response.send("erro");
+        })); 
+    
+  
+});
+
+exports.ModificarLivro = functions.https.onRequest((request, response) =>    {
+    response.send("Update");
+});
+
+exports.ExcluirLivro = functions.https.onRequest((request, response) =>    {
+    response.send("Delete");
+});
+
+exports.InserirLivro = functions.https.onRequest((request, response) =>    {
+    response.send("Insert");
+});
+
+//*********************************************************|
+//             CRUD da Reserva    - PARA APLICAÇÃO           |
+//*********************************************************|
+
+exports.ListarReservas = functions.https.onRequest((request, response) =>    {
+
+      var list = [];
+
+        var _resposta =  Reservas.get();
+
+        _resposta.then(function(resultado) { 
+            resultado.forEach(function(reserva) {                   
+                console.log(reserva.id, " => ", reserva.data());
+                list.push({id: reserva.id, dados: reserva.data()});                   
+            });
+
+            response.send({resultado:list});
+        })
+        .catch((e=>{
+            response.send("erro");
+        })); 
+    
+   
+});
+
+exports.CancelarReserva = functions.https.onRequest((request, response) =>    {
+    response.send("Update");
+});
+
+exports.AdicionarReserva = functions.https.onRequest((request, response) =>    {
+    response.send("Insert");
+});
+ 
