e64175a520e69276525f06e82208e9de195d5e40
Janynne Gomes <janynnegomes@gmail.com>
2018-11-21 23:07:45 -0200

    Criação de nova função na API para responder ao DialogFlow quando houver identificação de intenção do usuário

--- API/functions/index.js
+++ API/functions/index.js
@@ -7,3 +7,35 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Olá mundo! Esse é o minicurso de chatbot da Janynne Gomes");
 });
 
+exports.chatbot = functions.https.onRequest((request, response) => {
+    
+    // Objeto contendo parametros vindos do DialogFlow
+    let params = request.body.queryResult.parameters;
+    
+    // Ação identificada como intenção do usuário pelo DialogFlow
+    let action = request.body.queryResult.action;
+
+    switch(action)
+    {
+        case 'busca_livros':
+        {
+            // Implementar a lógica de leitura dos dados do Firestore
+            response.send({
+                fulfillmentText: `Ação detectada: Busca de livros.`
+            })
+            break;
+        }
+        case 'reserva_livro':{
+            // Implementar a lógica de inserção de dados do Firestore
+            response.send({
+                fulfillmentText: `Ação detectada: Reserva de livro.`
+            })
+            break;
+        }
+        default:
+            response.send({
+                fulfillmentText: `Nenhuma ação prevista detectada.`
+            })
+    } 
+
+   });   
